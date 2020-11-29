d48aa3b5f826111ed6919077112c6e274f52d924
Виктор Загоруйко <vzagoruyko@rbc.ru>
2020-11-29 13:38:05 +0500

    Add types

--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,35 @@ export namespace Api {
   export namespace ClientInfo {
     export interface Response {}
   }
+
+  export namespace Product {
+    export interface ProductCategories {
+      id: number;
+      slug: string;
+      title: string;
+    }
+
+    export interface Size {
+      size: string;
+      code: string;
+      quantity: number;
+    }
+
+    export interface Fibers {
+      title: string;
+      description: string;
+    }
+
+    export interface Item {
+      id: number;
+      slug: string;
+      title: string;
+      price: number;
+      description?: string;
+      isAvailable: boolean;
+      sizes: Size[];
+      fibers?: Fibers[];
+      photos: string[];
+    }
+  }
 }
